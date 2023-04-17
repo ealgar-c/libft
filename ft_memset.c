165c06dc4d1ffb0c6214b03059e168cf2f42003e
Enrique Algar Ceular <ealgar-c@c1r9s6.42malaga.com>
2023-04-17 17:34:20 +0200

    baby shower

--- ft_memset.c
+++ ft_memset.c
@@ -1,16 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:38:57 by ealgar-c          #+#    #+#             */
+/*   Updated: 2023/04/17 15:48:28 by ealgar-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
-void    *ft_memset(void *s, int c, size_t n)
+
+void	*ft_memset(void *b, int c, size_t len)
 {
-    size_t			counter;
-    unsigned char	*ptr;
+	size_t			counter;
+	unsigned char	*ptr;
 
 	counter = 0;
-	ptr = (unsigned char *)s;
-	while(counter < n)
+	ptr = (unsigned char *)b;
+	while (counter < len)
 	{
 		ptr[counter] = (unsigned char)c;
 		counter++;
 	}
-	return ptr;
+	return (b);
 }
